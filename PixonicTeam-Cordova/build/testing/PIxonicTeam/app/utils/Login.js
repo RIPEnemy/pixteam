fdea2a4c6557dc74ce6f8af24855bf58f9329f4a
Дарья Матюха <matyukha@pixonic.ru>
2016-02-11 16:46:58 +0300

    Cordova instead of Phonegap

---
+++ PixonicTeam-Cordova/build/testing/PIxonicTeam/app/utils/Login.js
@@ -0,0 +1,17 @@
+var scopes = 'https://www.googleapis.com/auth/calendar';
+
+
+//For WEB
+///*
+var clientId = '433633888093-3e1lolt86gbs88nks6cfmp20l22gq95b.apps.googleusercontent.com';
+var apiKey = 'AIzaSyA7jTupE0WjhL4yqvQEsy3wHZ9ONK8bgcw';
+//*/
+
+//for ANDROID
+/*
+var apiKey = 'AIzaSyASVoTq3eldXM1dIsuMP3d0MYEB_yEGJ_0';
+var clientId = '433633888093-oaqdtl7civ9m9bhh91joidq5hh0sivvd.apps.googleusercontent.com';
+var redirectUri = 'http://localhost';
+var callbackUri = 'http://localhost/callback';
+
+*/
--- PixonicTeam/build/temp/testing/PIxonicTeam/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/ifsogirl/PixonicTeam/app.js
